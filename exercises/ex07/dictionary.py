b8875a6c47a81dc2c18474ed44f0111278fd12d8
Lillian Seymour <lseymour@unc.edu>
2022-10-12 21:51:40 -0400

    new changes ex07

---
+++ exercises/ex07/dictionary.py
@@ -0,0 +1,43 @@
+"""EX07 - Dictionary Functions"""
+
+__author__ = "730463283"
+
+def invert(variables: dict[str, str]) -> dict[str, str]:
+    """This function will invert the values and keys of a dictionary."""
+    final_dict: dict[str,str] = {}
+    for key in variables:
+        if variables[key] in final_dict:
+            raise KeyError("Opps! You have entered duplicatr keys.")
+        else:
+            final_dict[variables[key]] = key
+    return final_dict
+
+
+def favorite_color(option: dict[str, str]) -> str:
+    """The function will assess a list of colors and determine the most popular."""
+    most_color: str = ""
+    values: list[str] = []
+    for name in option:
+        values.append(option[name])
+    new_dict: dict[str, int] = count(values)
+    max: int = 0
+    for color in new_dict:
+        if new_dict[color] > max:
+            max = new_dict[color]
+    for final in new_dict:
+        if new_dict[final] == max:
+            return final
+    return most_color
+            
+
+def count(number: list[str]) -> dict[str, int]:
+    """This function will count the number of times a value appears in an inputted list."""
+    empty: dict[str, int] = {}
+    i: int = 0
+    while i < len(number):
+        if number[i] in empty:
+            empty[number[i]] += 1
+        else:
+            empty[number[i]] = 1
+        i += 1
+    return empty
---
+++ exercises/ex07/dictionary_test.py
@@ -0,0 +1,58 @@
+"""EX07 - Dictionary Function Tests"""
+
+__author__ = "730463283"
+
+from dictionary import invert, favorite_color, count
+
+def test_invert_empty() -> None:
+    """Edge case for invert."""
+    xs: dict[str, str] = {}
+    assert invert(xs) == {}
+
+
+def test_invert_many() -> None:
+    """Use case 1 for invert."""
+    xs: dict[str, str] = {"a":"b", "l":"m", "d":"e"}
+    assert invert(xs) == {"b":"a", "m":"l", "e":"d"}
+
+
+def test_invert_many_again() -> None:
+    """Use case 2 for invert."""
+    xs: dict[str, str] = {"ella":"lilly", "gracie":"kate", "marion":"loulie"}
+    assert invert(xs) == {"lilly":"ella", "kate":"gracie", "loulie":"marion"}
+
+
+def test_favorit_color_empty() -> None:
+    """Edge case for favorite_color."""
+    xs: dict[str, str] = {}
+    assert favorite_color(xs) == ""
+
+
+def test_favorite_color_many() -> None:
+    """Use case 1 for favorite_color."""
+    xs: dict[str, str] = {"Ella": "green", "Lilly": "green", "Loulie": "blue"}
+    assert favorite_color(xs) == "green"
+
+
+def test_favorite_color_many_again() -> None:
+    """Use case 2 for favorite_color."""
+    xs: dict[str, str] = {"Campbell": "orange", "Caroline": "pink", "Mary": "pink", "Blair": "yellow"}
+    assert favorite_color(xs) == "pink"
+
+
+def test_count_empty() -> None:
+    """Edge case for count."""
+    xs: list[str] = []
+    assert count(xs) == {}
+
+
+def test_count_many() -> None:
+    """Use case 1 for count."""
+    xs: list[str] = ["flower", "power", "shower", "flower", "power"]
+    assert count(xs) == {"flower": 2, "power": 2, "shower": 1}
+
+
+def test_count_many_again() -> None:
+    """Use case 2 for count."""
+    xs: list[str] = ["cup", "cup", "cup", "mug", "plate", "mug", "bowl"]
+    assert count(xs) == {"cup": 3, "mug": 2, "plate": 1, "bowl": 1}
